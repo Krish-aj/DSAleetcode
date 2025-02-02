09c96946ba5fce7ad6b16168725f20d073e046f1
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-02-02 14:10:17 +0530

    Create twosum.c
    kkk

---
+++ twosum.c
@@ -0,0 +1,21 @@
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int i,k;
+    int* Size=(int*)malloc(numsSize*sizeof(int));
+    for(i=0;i<numsSize-1;i++){
+        for(k=i+1;k<numsSize;k++){
+            if(nums[i]+nums[k]==target){
+                Size[0]=i;
+                Size[1]=k;
+                *returnSize=2;
+                return Size;
+                
+            }
+        }
+       
+    }
+    free(Size);
+    return NULL;
+}
+    
+    
