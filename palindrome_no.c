2beaaf6269e8eb6d70c7c9b1ee5c478b30bcb25a
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-09-10 00:28:13 +0530

    Update palindrome_no.c

--- palindrome_no.c
+++ palindrome_no.c
@@ -1,13 +1,13 @@
 bool isPalindrome(int x) {
-    // int duplicate=x;
+   
     int i,j,count=0;
     int length = (x == 0) ? 1 : (int)log10(x) + 1;
-    char s[12];
-    sprintf(s,"%d",x);
-    for(i=0,j=length-1;i<=length-1,j>=0;i++,j--){
-        if(s[i]==s[j]) count++;
+    char s[12];              //12 beacuse integer range is 11 digits +1 null char 
+    sprintf(s,"%d",x);       // convert into string
+    for(i=0,j=length-1;i<=length-1,j>=0;i++,j--){       // i start from 0 and j from last then compare 
+        if(s[i]==s[j]) count++;           // if matches then count ++
     }
-    if(count==length){
+    if(count==length){      // if palindrome then count will be equal to length because for loop will execute length times.
         return true;
     }else 
         return false;
